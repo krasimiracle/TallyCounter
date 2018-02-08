5fa0fe29c890d28cc487f3888c7ccd572ec87f3b
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-09 00:24:30 +0200

    Added Room API dependency, pojo for Tally

--- app/src/main/java/com/stoyanov5/tallycounter/data/Tally.java
+++ app/src/main/java/com/stoyanov5/tallycounter/data/Tally.java
@@ -4,15 +4,36 @@ package com.stoyanov5.tallycounter.data;
  * Created by B3f0r on 08-Feb-18.
  */
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 /**
  * Immutable model class for a Tally.
  */
 @Entity(tableName = "tallies")
 public final class Tally {
 
-    private int counter;
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "entryid")
+    private final String id;
 
+    @NonNull
+    @ColumnInfo(name = "counter")
+    private int counter = 0;
+
+    @Nullable
+    @ColumnInfo(name = "name")
     private String Name;
+
+    public Tally(@NonNull String id, @Nullable String name, @NonNull int counter){
+        this.id = id;
+        this.Name = name;
+        this.counter = counter;
+    }
 
     public String getName() {
         return Name;
