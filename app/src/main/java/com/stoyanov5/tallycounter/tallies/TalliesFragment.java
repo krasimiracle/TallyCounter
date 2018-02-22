860ca3b7667d08e3ef2d19993cc9eaa83f45519d
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-22 14:43:05 +0200

    Added Adapter and ItemListener for tallies

--- app/src/main/java/com/stoyanov5/tallycounter/tallies/TalliesFragment.java
+++ app/src/main/java/com/stoyanov5/tallycounter/tallies/TalliesFragment.java
@@ -7,16 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 import com.stoyanov5.tallycounter.data.Tally;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by B3f0r on 14-Feb-18.
  */
 
 public class TalliesFragment extends Fragment implements TalliesContract.View {
+
+    private TalliesContract.Presenter talliesPresenter;
+
+    private TalliesAdapter talliesAdapter;
 
     // Required empty constructor
     public TalliesFragment() {
@@ -30,6 +38,13 @@ public class TalliesFragment extends Fragment implements TalliesContract.View {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        talliesAdapter = new TalliesAdapter(new ArrayList<Tally>(0), tallyItemListener);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        talliesPresenter.start();
     }
 
     @Nullable
@@ -39,13 +54,8 @@ public class TalliesFragment extends Fragment implements TalliesContract.View {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public void setPresenter(TalliesContract.Presenter presenter) {
-
+        talliesPresenter = checkNotNull(presenter);
     }
 
     @Override
@@ -60,6 +70,67 @@ public class TalliesFragment extends Fragment implements TalliesContract.View {
 
     @Override
     public void showAddTally() {
+        // Intent to addTallyActivity
+    }
 
+    TallyItemListener tallyItemListener = new TallyItemListener() {
+        @Override
+        public void onTallyClicked(Tally clickedTally) {
+            // presenter.openTallyDetail
+        }
+
+        @Override
+        public void onTallyIncremented(Tally incrementedTally) {
+            // presenter.incrementTally
+        }
+
+        @Override
+        public void onTallyDecremented(Tally decrementedTally) {
+            // presenter.decrementTally
+        }
+    };
+
+    private static class TalliesAdapter extends BaseAdapter {
+
+        private List<Tally> tallies;
+        private TallyItemListener tallyItemListener;
+
+        public TalliesAdapter(List<Tally> tallies, TallyItemListener tallyItemListener) {
+            setList(tallies);
+            this.tallyItemListener = tallyItemListener;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+
+        private void setList(List<Tally> tallies) {
+            this.tallies = checkNotNull(tallies);
+        }
+    }
+
+    public interface TallyItemListener {
+
+        void onTallyClicked(Tally clickedTally);
+
+        void onTallyIncremented(Tally incrementedTally);
+
+        void onTallyDecremented(Tally decrementedTally);
     }
 }
