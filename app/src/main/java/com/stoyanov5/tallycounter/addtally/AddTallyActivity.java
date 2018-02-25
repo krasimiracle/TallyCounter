c0facea5f73d4ceac0f1bac8d0aa7bd8962873e6
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-25 17:25:45 +0200

    Added carview dependency, AddTallyActivity exnteds appcompat, added basic methods in oncreate

--- app/src/main/java/com/stoyanov5/tallycounter/addtally/AddTallyActivity.java
+++ app/src/main/java/com/stoyanov5/tallycounter/addtally/AddTallyActivity.java
@@ -1,8 +1,24 @@
 package com.stoyanov5.tallycounter.addtally;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by B3f0r on 23-Feb-18.
  */
 
-public class AddTallyActivity {
+public class AddTallyActivity extends AppCompatActivity {
+
+    private AddTallyPresenter addTallyPresenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.addtally_activity);
+
+        //AddTallyFragment addTallyFragment = getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+
+        //addTallyPresenter = new AddTallyPresenter(tallyId, Injection.provideTalliesRepository(getApplicationContext()), addTallyFragment);
+    }
 }
